b1d66ca2acf8e6d5637650d81a6918714b73186c
Petaaar <psihosocial@abv.bg>
2018-05-05 10:52:16 +0300

    File deletion,creation and program running separated.

---
+++ SASH/IO/Create.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+
+namespace SASH.IO
+{
+    class Create
+    {
+        private string path;
+
+        /// <summary>
+        /// Creates a file / folder in the given in the <paramref name="arguments"/> directory.
+        /// If there is not given any directory argument, the file is created in the current one.
+        /// </summary>
+        /// <param name="arguments">The arguments to the command.</param>
+        public Create(string path, string[] arguments)
+        {
+            this.path = path;
+
+            string filename = string.Empty;
+            string destination = string.Empty;
+            string[] filesArr = Array.Empty<string>();
+            int argsLen = arguments.Length;
+
+            if (argsLen == 1) filename = arguments[0];
+
+            if (argsLen == 3 && arguments[1] == "in")
+            {
+                filename = arguments[0];
+                if (Directory.Exists(arguments[2]) || arguments[2] == nameof(path))
+                {
+                    string argsLast = arguments[2];
+
+                    if (argsLast[argsLast.Length - 1] == @"\".ToCharArray()[0])
+                        destination = argsLast;
+                    else
+                        destination = argsLast + @"\";
+                }
+                else
+                {
+                    Internal.Error("Destination does not exist!");
+                    //TODO: CREATING A NEW DESTINATION!!!
+                    Internal.Starter(this.path);
+                }
+            }
+
+            if (destination == nameof(path) || argsLen == 1)
+                destination = this.path;
+
+            if (IsListOfFiles(filename))
+            {
+                filesArr = filename.Split(new char[] { ',', '{', '}' }, StringSplitOptions.None);
+
+                var files = filesArr.ToList();
+
+                //fixing the "path" issue.
+                files.Remove(files[0]);
+                files.Remove(files[files.Count - 1]);
+
+                System.Threading.Tasks.Parallel.For(0, files.Count,
+                    x => CreateFile(destination + files[x]));
+
+                Internal.Starter(this.path);
+            }
+            else
+            {
+                if (File.Exists(destination + filename))
+                {
+                    Internal.Error("File already exists in the specified destination!");
+                    Internal.Starter(this.path); //save from cross-thread operation!
+                }
+                else
+                {
+                    CreateFile(destination + filename);
+                    Internal.Starter(this.path);
+                }
+            }
+
+            if (argsLen == 1) CreateFile(destination + filename);
+
+            Internal.Starter(this.path);
+
+        }
+    #pragma warning restore
+
+
+        /// <summary>
+        /// Creates a file with given <paramref name="filename"/>
+        /// </summary>
+        /// <param name="filename">The name of the file to be created.</param>
+        private void CreateFile(string filename)
+        {
+            try
+            {
+                File.Create(filename);
+                Console.WriteLine($"Created a new file in {filename}!");
+                Internal.Starter(this.path);
+            }
+            catch (IOException) {; }
+        }
+
+        /// <summary>
+        /// Determines whenever a string is a list of arguments needed for
+        /// the <see cref="Create(string[])"/> function.
+        /// </summary>
+        /// <param name="argument"></param>
+        /// <returns>boolean</returns>
+        private static bool IsListOfFiles(string argument)
+            => argument[0] == '{' & argument[argument.Length - 1] == '}';
+
+    }
+}
---
+++ SASH/IO/Delete.cs
@@ -0,0 +1,151 @@
+﻿using System.IO;
+
+namespace SASH.IO
+{
+    class Delete
+    {
+        private string path;
+
+        /// <summary>
+        /// Checks if a specified <paramref name="path"/> exists.
+        /// </summary>
+        /// <param name="path">The path specified.</param>
+        /// <returns>boolean</returns>
+        private static bool CheckPath(string path)
+            => Directory.Exists(path);
+
+        /// <summary>
+        /// Deletes s file or many files in a specified or the current directory.
+        /// </summary>
+        /// <param name="arguments">An string array for the arguments needed.</param>
+        public Delete(string path, string[] arguments)
+        {
+            this.path = path;
+
+            if (arguments.Length == 0)
+                Internal.Error("No arguments given. What to run!?");
+            if (arguments.Length <= 2 || arguments.Length > 3)
+                Internal.Error("Too many or too few arguments given!");
+            if (arguments[1] != "in")
+                Internal.Error($"Expected keyword IN in the place of \"{arguments[1]}\".");
+
+            var file = arguments[0];
+            var destination = string.Empty;
+
+
+            if (arguments[2] == nameof(this.path))
+                destination = this.path;
+            else
+                destination = arguments[2];
+
+            var dirInfo = new DirectoryInfo(destination);
+
+
+
+            try
+            {
+                if (!CheckPath(destination))
+                    Internal.Error($"The destination \"{destination}\" does not exist!");
+                else if (dirInfo.GetFileSystemInfos().Length == 0)
+                    Internal.Error($"The destination \"{destination}\" is empty!");
+            }
+            catch (FileNotFoundException)
+            {
+                Internal.Error($"The destination \"{destination}\" does not exist!");
+                Internal.Starter(this.path);
+            }
+
+            if (file == "*")
+            {
+                try
+                {
+                    DeleteFullContent(destination);
+                }
+                catch (IOException)
+                {
+                    Internal.Error("Directory not found. Retrying..");
+                    try { DeleteFullContent(destination); }
+                    catch (IOException)
+                    {
+                        Internal.Error("FAILED! PLEASE RESTART!");
+                    }
+                    Internal.Starter(this.path);
+                }
+            }
+            else
+            {
+                if (destination[destination.Length - 1] != @"\".ToCharArray()[0])
+                    destination = destination + @"\";
+
+                if (File.Exists(destination + file) && !IsFileLocked(destination + file))
+                {
+                    try
+                    {
+                        file = destination + file;
+                        File.Delete(file);
+                        Internal.Starter(this.path);
+                    }
+                    catch (IOException)
+                    {
+                        Internal.Error($"The file \"{file}\" is used by another program.");
+                        TryKill(file);
+                        Internal.Starter(this.path);
+                    }
+                }
+                else
+                {
+                    if (destination[destination.Length - 1] != @"\".ToCharArray()[0])
+                        destination += @"\";
+
+                    File.Delete(file);
+                    Internal.Starter(this.path);
+                    Internal.Starter(this.path);
+                }
+            }
+
+            Internal.Sleep(2500);
+        }
+
+        /// <summary>
+        /// Deletes EVERYTHING in a given <paramref name="destination"/>.
+        /// </summary>
+        /// <param name="destination">The destination.</param>
+        private void DeleteFullContent(string destination)
+        {
+            var directoryInfo = new DirectoryInfo(destination);
+
+            foreach (string item in Directory.GetFiles(destination))
+                File.Delete(item);
+
+            Internal.Starter(this.path);
+        }
+
+
+        private static bool IsFileLocked(string path)
+        {
+            bool res = false;
+            FileInfo info = new FileInfo(path);
+
+            try
+            {
+                var stream = info.Open(FileMode.Open, FileAccess.ReadWrite);
+                stream.Dispose();
+            }
+            catch (IOException) { res = true; }
+            return res;
+        }
+
+        private static void TryKill(string file)
+        {
+            File.Delete(file);
+            var process = new System.Diagnostics.Process();
+            process.StartInfo.FileName = file;
+            process.Start();
+            process.WaitForExit(500); // wait a minute....
+            if (!process.HasExited)
+                process.Kill(); // *KILL IT!*
+
+            process.Dispose();
+        }
+    }
+}
