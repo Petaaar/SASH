f6be2e4b08d64610b85c51733bb57f7930f2a770
Petaaar <psihosocial@abv.bg>
2018-05-08 17:35:55 +0300

    Written the base logic of the "Hidden" class.

--- SASH/Hidden/Help.cs
+++ SASH/Hidden/Help.cs
@@ -5,6 +5,34 @@
     /// </summary>
     public struct Help
     {
+        /// <summary>
+        /// Contains short descriptions of all commands.
+        /// </summary>
+        private static readonly System.Collections.Generic.Dictionary<string, string>
+            commandHelpShort = new System.Collections.Generic.Dictionary<string, string>
+            {
+                {"copy","=> Copies a file/directory in another/the boot directory."},
+                {"create","=> Creates a new file(s) in a specified/the boot directory."},
+                {"delete","=> Deletes a file(s)/directory in a specified/the boot directory"},
+                {"run","=> Starts a new process to run an external application."}
+            };
 
+        /// <summary>
+        /// Determines if a help for given command exists.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        private void Run(string command)
+        {
+            command = command.ToLower();
+
+            if (command == "copy" || command == "create" || command == "delete" || command == "run")
+                System.Console.WriteLine(commandHelpShort[command]);
+            else Internal.Error($"Could not find help for command \"{command}\"!");
+        }
+
+        public Help(string command)
+        {
+            Run(command);
+        }
     }
 }
